6c7e4fa568a66bfc50e68eda2c47e082ac4aab3b
Furkan <furkanakaldev@gmail.com>
2024-02-01 12:37:48 +0300

    init: some fundamental functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+
+}
+
+fn generate_keys(P_x: u64, P_y: u64, n: u64) -> (u64, u64, u64) {
+
+}
+
+fn curve(x: u64) -> u64 {
+
+}
+
+fn find_prime_order(p: u64, P_x: u64, P_y: u64) -> u64 {
+
+}
+
+fn generate_elliptic_cyclic_subgroup(p: u64, P_x: u64, P_y: u64, n: u64) -> [(u64, u64); (n-1)] {
+    
 }
